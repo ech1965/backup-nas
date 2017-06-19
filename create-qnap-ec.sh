a5806e94d8f5ecacb567ee1971eb435e1a4a9d81
Etienne Charlier <etienne@charlier.info>
2017-06-19 22:36:38 +0200

    Fixes

--- create-qnap-ec.sh
+++ create-qnap-ec.sh
@@ -4,32 +4,18 @@ USER_ID=1001
 GROUP_ID=100
 NAME=BACKUP-EC
 
-# Top level folder in backup area
-BACKUP_HOME=/share/Backups/$USER_NAME
-
-# Folder for backup jobs (scripts & cron)
-JOBS_HOME=$BACKUP_HOME/Configuration/jobs
-
-# Folder for configuration files
-CONFIG_HOME=$BACKUP_HOME/Configuration/config
-
-# User's home in NAS 
-HOME_FOLDER=/share/homes/$USER_NAME
-PERSO_FOLDER=$BACKUP_HOME/perso
-
-# Folder for pref-dir's
-PREF_DIR_FOLDER=$BACKUP_HOME/pref-dirs
-
-# Reports generated from cron
-REPORTS_HOME=/share/Web/backup-ec/reports/
-
-# Folder for restoration
-RESTORE_POINT=$BACKUP_HOME/RESTORE
+USER_PERSO="/share/persos/ec"          # Le répertoire personnel non visible samba pour cet utilisateur
+USER_HOME="/share/homes/ec"            # Le répertoire home de l'utilisateur sur le NAS
 
 
+CONFIG="$USER_PERSO/config"            # Répertoire qui contient des données de config ( clés SSH...)
+DATA_HOME="$USER_HOME"                 # Répertoire qui contient les données à backuper
+DATA_PERSO="$USER_PERSO/dataperso"     # Répertoire qui contient les données à backuper
+PREF_DIRS="$USER_PERSO/pref-dirs"      # Répertoire qui contient les -pref-dir pour duplicacy
+REPORTS="/share/Web/backup-ec/reports" # Répertoire dans lequel on écrira les rapports d'exécution
+RESTORE="$USER_PERSO/restore"          # Répertoire dans lequel on Lancera les restore
 
 SSH_PORT=50086
-#WEB_PORT=50087
 
 docker kill $NAME
 docker rm  $NAME
@@ -40,13 +26,12 @@ docker pull ech1965/backup-nas:latest
 docker create \
   --name=$NAME \
   -v /etc/localtime:/etc/localtime \
-  -v $JOBS_HOME:/jobs  \
-  -v $CONFIG_HOME:/config \
-  -v $HOME_FOLDER:/datahome  \
-  -v $PERSO_FOLDER:/dataperso  \
-  -v $PREF_DIR_FOLDER:/pref-dirs  \
-  -v $REPORTS_HOME:/reports \
-  -v $RESTORE_POINT:/restore \
+  -v $CONFIG:/config \
+  -v $DATA_HOME:/datahome  \
+  -v $DATA_PERSO:/dataperso  \
+  -v $PREF_DIRS:/pref-dirs  \
+  -v $REPORTS:/reports \
+  -v $RESTORE:/restore \
   -p $SSH_PORT:22 \
   -e PGID=$GROUP_ID -e PUID=$USER_ID -e PUSER=$USER_NAME  \
   ech1965/backup-nas:latest
